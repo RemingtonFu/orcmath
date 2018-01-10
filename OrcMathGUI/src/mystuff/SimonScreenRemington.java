cfe6291d14f43542bd3d599c5daa11b8f0277008
Remington <Remington@Renovo.fios-router.home>
2018-01-10 00:13:26 -0500

    Completed Strong

--- OrcMathGUI/src/mystuff/SimonScreenRemington.java
+++ OrcMathGUI/src/mystuff/SimonScreenRemington.java
@@ -7,64 +7,71 @@ import java.util.List;
 import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.TextArea;
+import guiTeacher.components.TextLabel;
+import guiTeacher.interfaces.FileRequester;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.ClickableScreen;
+import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class SimonScreenRemington extends ClickableScreen implements Runnable{
 
-	private TextLabel text;
-	private ButtonInterfaceRemington[] button;
+	private ButtonInterfaceRemington[] buttons;
 	private ProgressInterfaceRemington progress;
-	private ArrayList<MoveInterfaceRemington> sequence;
+	private TextLabel text;
+	private ArrayList<MoveInterfaceRemington> buttonOrder = new ArrayList<MoveInterfaceRemington>();
+	private int count;
+	private int roundNumber = 1;
+	boolean acceptingInput = false;
+	int lastSelectedButton;
 	
-	private int roundNumber;
-	private boolean acceptingInput;
-	private int sequenceIndex;
-	private int lastSelectedButton;
-	private TextLabel label;
-	
-	
-	public SimonScreenRemington(int width, int height) {
-		super(width, height);
+	public SimonScreenRemington(int width,int height) {
+		super(width,height);
 		Thread app = new Thread(this);
 		app.start();
-		}
+	}
 
-	@Override
-	public void run() {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		addButtons();
-		for(SimonScreenRemington b: buttons){ 
-		    viewObjects.add(b); 
+		for(ButtonInterfaceRemington b: buttons) {
+			viewObjects.add(b);
 		}
 		progress = getProgress();
-		label = new TextLabel(130,230,300,40,"Let's play Simon!");
-		sequence = new ArrayList<MoveInterfaceRemington>();
-		//add 2 moves to start
+		buttonOrder = new ArrayList<MoveInterfaceRemington>();
 		lastSelectedButton = -1;
-		sequence.add(randomMove());
-		sequence.add(randomMove());
+		buttonOrder.add(randomMove());
+		buttonOrder.add(randomMove());
 		roundNumber = 0;
 		viewObjects.add(progress);
-		viewObjects.add(label);
-
-		
+		viewObjects.add(text);
 	}
 
 	private MoveInterfaceRemington randomMove() {
-	    int bIndex = (int)(Math.random()*buttons.length);
-	    while(bIndex == lastSelectedButton){ 
-	        bIndex = (int)(Math.random()*buttons.length);
-	    }
-	    return getMove(bIndex);
+		int random = (int)(Math.random()*buttons.length);
+		while(random == lastSelectedButton) {
+			random = (int)(Math.random()*buttons.length);
+		}
+		return getMove(random);
 	}
 
-	/**
-	Placeholder until partner finishes implementation of ProgressInterface
-	*/
+	private MoveInterfaceRemington getMove(int random) {
+		return null;
+	}
+
 	private ProgressInterfaceRemington getProgress() {
 		// TODO Auto-generated method stub
 		return null;
@@ -72,14 +79,50 @@ public class SimonScreenRemington extends ClickableScreen implements Runnable{
 
 	private void addButtons() {
 		int numberOfButtons = 4;
-		button = new ButtonInterfaceRemington[numberOfButtons];
-		Color[] color = null;
-		color[1] = Color.blue;
-		color[2] = Color.green;
-		color[3] = Color.red;
-		color[4] = Color.pink;
+		buttons = new ButtonInterfaceRemington[numberOfButtons];
+		Color[] colors = null;
+		colors[1] = Color.blue;
+		colors[2] = Color.red;
+		colors[3] = Color.green;
+		colors[4] = Color.pink;
+		for(int i = 0; i < numberOfButtons; i++) {
+			final ButtonInterfaceRemington b = getAButton();
+			b.setColor(colors[i]);
+			b.setX(40);
+			b.setY(40);
+			b.setAction(new Action(){
+				
+				public void act() {
+					if(acceptingInput) {
+						Thread blink = new Thread(new Runnable() {
+							public void run() {
+								b.highlight();
+								try {
+									Thread.sleep(800);
+								} catch(InterruptedException e) {
+									e.printStackTrace();
+								}
+								b.dim();
+							}
+						});
+						blink.start();
+					}
+				}
+				
+			});
+			buttons[i] = b;
+		}
+	}
+
+	private ButtonInterfaceRemington getAButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
-		for(int i )
 	}
 
 }
